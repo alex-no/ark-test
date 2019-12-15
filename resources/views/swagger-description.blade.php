d2d27468a05b44226df7690887e8d2cba25d86ab
alexandr-no1 <alexandr.no1@gmail.com>
2019-12-15 05:09:32 +0200

    Show swagger [2]

--- resources/views/swagger-description.blade.php
+++ resources/views/swagger-description.blade.php
@@ -1,14 +1,7 @@
-This project designed to demonstrate working of <b>ronasit/laravel-swagger</b> plugin.
-
-Here is project description from <b>swagger-description.blade.php</b>
 <div class="swagger-ui">
     <div class="opblock-body">
         <pre>
             Test swagger ARK blockchain
         </pre>
     </div>
-</div>
-Or some image
-<div style="display: flex; justify-content: center; width: 100%">
-    <img src="/img/hqdefault.jpg"/>
 </div>

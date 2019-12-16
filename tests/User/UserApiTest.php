67383816bd73c45b5f010eb108ea3ca7ae25fae8
alexandr-no1 <alexandr.no1@gmail.com>
2019-12-16 03:02:16 +0200

    Make base swagger methods

--- tests/User/UserApiTest.php
+++ tests/User/UserApiTest.php
@@ -6,77 +6,25 @@ use Tests\TestCase;
 
 class UserApiTest extends TestCase
 {
-
-
-    /**
-     *
-     * @return array
-     */
-    public function urlProviderSuccessGet()
-    {
-        return [
-            ['/api/users'],
-            ['/api/users/1'],
-        ];
-    }
+    use \Tests\SaveSwagger;
 
     /**
-     *
-     * @return array
-     */
-    public function urlProviderErrorGet()
-    {
-        return [
-            ['/api/users/111'],
-        ];
-    }
-
-    /**
-     *
-     * @return array
-     */
-    public function urlProviderSuccessPost()
-    {
-        return [
-            ['post', '/api/users', ['first_name' => 'Xxxx3', 'last_name' => 'Yyyyy2', 'phone' => '000000000', 'email' => 'email345@dfdf', 'password' => '12345678']],
-        ];
-    }
-
-    /**
-     * A basic test example.
-     * @dataProvider urlProviderSuccessGet
+     * A test login user
      * @return void
      */
-    public function testSuccessGet($uri)
+    public function testLogin()
     {
-        /* @var $responce \Illuminate\Foundation\Testing\TestResponse */
-        $responce = $this->json('get', $uri);
-        //print "\n" . $responce->getContent() . "\n";
-        $responce->assertOk();
-    }
-    /**
-     * A basic test example.
-     * @dataProvider urlProviderErrorGet
-     * @return void
-     */
-    public function testErrorGet($uri)
-    {
-        /* @var $responce \Illuminate\Foundation\Testing\TestResponse */
-        $responce = $this->json('get', $uri);
-        //print "\n" . $responce->getContent() . "\n";
-        $responce->assertNotFound();
-    }
-    /**
-     * A basic test example.
-     * @dataProvider urlProviderSuccessPost
-     * @return void
-     */
-    public function otestSuccessPost($method, $uri, array $data)
-    {
-        /* @var $responce \Illuminate\Foundation\Testing\TestResponse */
-        $responce = $this->json($method, $uri, $data);
-        print "\n" . $responce->getContent() . "\n";
-        $responce->assertOk();
-    }
+        $data= [
+            'email'      => 'test1@mail.com',
+            'password'   => '12345678',
+        ];
 
+        /* @var $responce \Illuminate\Foundation\Testing\TestResponse */
+        $responce = $this->json('post', '/api/auth/login', $data);
+
+        $content = $responce->getContent();
+        print "\n" . $content . "\n";
+        $this->assertNotTrue(boolval(strstr($content, 'errors')));
+        //$responce->assertOk();
+    }
 }
